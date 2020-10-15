48ece091f94f522eeeb8fab20961e2f8651f9d6d
Yana Malanii <miramis134@gmail.com>
2020-10-15 15:15:46 +0300

    refactoring

--- index.js
+++ index.js
@@ -6,25 +6,21 @@ const cardTitle = document.querySelector('#cardTitle');
 const cardDesc = document.querySelector('#cardDescription');
 const cardNumber = document.querySelector('#cardNumber');
 const shownCards = document.querySelector('.single-img-wrapper');
-
-
-exploreBtn.addEventListener('click', () => {
-        gsap.to(".circle", {padding: 1500, x: -5, y: -350, duration: 1});
-        gsap.to(".content", {y: -800, opacity: 0.3, duration: 0.1,});
-        gsap.to(".main-img", {y: 300, opacity: 0, duration: 0.5,});
-        gsap.fromTo(".content-card", {y: 300, opacity: 0}, {display: 'block', duration: 2, y: 0, opacity: 1});
-    gsap.fromTo(".top-img",  {y: 0, opacity: 1, },{y: -100, opacity: 0, duration: 1});
-        gsap.fromTo(".single-img-wrapper", {y: 600, x: 600, transform: "skew(15deg, -15deg)", opacity: 0},
-            {duration: 0.5, display: "flex", y: 0, x: 0, transform: "skew(0deg, -0deg)", opacity: 1, onComplete: fromHomePage});
-    }
-);
-const cards = [{title: "Cat Witch",
+const elementsFromHomePage = [{className: ".main-img", value: "none"}, {className: ".content", value: "none"}, {className: ".top-img", value: "none"}];
+const elementsToHomePage = [{className: ".content-card", value: "none"}, {className: ".single-img-wrapper", value: "none"},
+    {className: ".main-img", value: "block"}, {className: ".content", value: "block"}, {className: ".top-img", value: "block"}];
+const backGroundsToHomePage = [{className: ".wrapper", value: "#161010"}, {className: ".circle", value: "#94ceed"}];
+const backGroundsFromHomePage = [{className: ".wrapper", value: "#94ceed"}];
+const cards = [{
+    title: "Cat Witch",
     img: "img/card1.png",
     description: 'It was popularised in the 1960s with the release of Letroset sheets ' +
         'containing Lorem ipsum passages,and more recently with desktop publishing software',
     number: "01",
-    backColor: " #94ceed"},
-    {title: "Kid Scull",
+    backColor: " #94ceed"
+},
+    {
+        title: "Kid Scull",
         img: "img/card2.png",
         description: "Embarrassing hidden in the middle of text. All the\n" +
             "Lorem Ipsum generators on the Internet tend to\n" +
@@ -42,26 +38,31 @@ const cards = [{title: "Cat Witch",
         backColor: " #ff8c48"
     },
 ];
-
-function backgroundAnimation(param) {
-    document.querySelector('.circle').style.backgroundColor = param.backColor;
-    gsap.fromTo(".circle", {y: 500, x: -500, opacity: 0, padding: 30},
-        {y: 100, x: 300, opacity: 1, padding: 1500, duration: 1.2});
-}
-
-function putCardsInfo(param) {
-
-    gsap.fromTo(".content-card", {opacity: 0}, {
-        opacity: 1, duration: 10, function() {
-            cardTitle.innerHTML = param.title;
-            cardDesc.innerHTML = param.description;
-            cardNumber.innerHTML = param.number;
-            backgroundAnimation(param)
-        }
-    });
-}
+exploreBtn.addEventListener('click', () => {
+        gsap.to(".circle", {padding: 1500, x: -5, y: -350, duration: 1});
+        gsap.to(".content", {y: -800, opacity: 0.3, duration: 0.1,});
+        gsap.to(".main-img", {y: 300, opacity: 0, duration: 0.5,});
+        gsap.fromTo(".content-card", {y: 300, opacity: 0}, {display: 'block', duration: 2, y: 0, opacity: 1});
+        gsap.fromTo(".top-img", {y: 0, opacity: 1,}, {y: -100, opacity: 0, duration: 1});
+        gsap.fromTo(".single-img-wrapper", {y: 600, x: 600, transform: "skew(15deg, -15deg)", opacity: 0},
+            {duration: 0.5, display: "flex", y: 0, x: 0, transform: "skew(0deg, -0deg)", opacity: 1, onComplete: fromHomePage});
+    }
+);
 
 
+carouselBtn.addEventListener('click', () => {
+    let currentCardSrc = shownCards.children[0].getAttribute("src");
+    if (currentCardSrc === cards[cards.length - 1].img) {
+        card.src = cards[0].img;
+        cardNext.src = cards[1].img;
+        toHomePage();
+    } else {
+        gsap.fromTo(cardNext, {y: 600, x: 600, opacity: 0},
+            {duration: 1, display: "flex", y: 0, x: 0, opacity: 1});
+        gsap.fromTo(card, {y: 180, x: 550, opacity: 1,},
+            {duration: 1, display: "flex", y: 0, x: 0, opacity: 1, onComplete: changeCards(cards)});
+    }
+});
 function changeCards(cards) {
     const currentCardSrc = shownCards.children[0].getAttribute("src");
     for (let i = 0; i < cards.length; i++) {
@@ -79,65 +80,52 @@ function changeCards(cards) {
     }
 }
 
-carouselBtn.addEventListener('click', () => {
-    let currentCardSrc = shownCards.children[0].getAttribute("src");
-
-    if (currentCardSrc === cards[cards.length - 1].img) {
-        toHomePage();
-    } else {
-        gsap.fromTo(cardNext, {y: 600, x: 600, opacity: 0},
-            {duration: 1, display: "flex", y: 0, x: 0, opacity: 1});
-        gsap.fromTo(card, {y: 180, x: 550, opacity: 1,},
-            {duration: 1, display: "flex", y: 0, x: 0, opacity: 1, onComplete: changeCards(cards)
-            });
-    }
-});
-
-const elementsFromHomePage = [{className: ".main-img", value: "none"}, {className: ".content", value: "none"}, {className: ".top-img", value: "none"}];
 
 function fromHomePage() {
-
-    document.querySelector('.circle').classList.remove("circle-homepage");
-    document.querySelector('.circle').classList.add("circle-position-with-card");
-
-    backgroundColor('.wrapper', '#94ceed');
+    mainStyleCircle('.circle',"circle-homepage","circle-position-with-card", );
+    backgroundColor(backGroundsFromHomePage);
     displayShow(elementsFromHomePage);
 }
 
-
-const elementsToHomePage = [{className: ".content-card", value: "none"}, {className: ".single-img-wrapper", value: "none"},
-    {className: ".main-img", value: "block"}, {className: ".content", value: "block"},{className: ".top-img", value: "block"}];
-
-
 function toHomePage() {
-    document.querySelector('.circle').classList.remove("circle-position-with-card");
-    document.querySelector('.circle').classList.add("circle-homepage");
-
-    gsap.fromTo(".circle", {y: -400, x: 200, padding: 1500},
-        {x: 0, y: 0, padding: "30vh", duration: 1}
-    );
-
+    gsap.fromTo(".circle", {y: -400, x: 200, padding: 1500}, {x: 0, y: 0, padding: "30vh", duration: 1});
     gsap.to(".main-img", {y: 0, opacity: 1, duration: 1.5,});
     gsap.to(".content", {y: 0, opacity: 1, duration: 1,});
-    gsap.fromTo(".top-img",  {y: -100, opacity: 0, },{y: 0, opacity: 1, duration: 2,});
-
-    card.src = cards[0].img;
-    cardNext.src = cards[1].img;
-
-    cardTitle.innerHTML = cards[0].title;
-    cardDesc.innerHTML = cards[0].description;
-    cardNumber.innerHTML = cards[0].number;
-
-    backgroundColor('.wrapper', '#161010');
-    backgroundColor('.circle', '#94ceed');
+    gsap.fromTo(".top-img", {y: -100, opacity: 0,}, {y: 0, opacity: 1, duration: 2,});
+    mainStyleCircle('.circle',"circle-position-with-card", "circle-homepage");
+    putCardsText(cards[0]);
+    backgroundColor(backGroundsToHomePage);
     displayShow(elementsToHomePage)
 }
 
+function putCardsText(card) {
+    cardTitle.innerHTML = card.title;
+    cardDesc.innerHTML = card.description;
+    cardNumber.innerHTML = card.number;
+}
 
-function backgroundColor(elClass, color) {
-    document.querySelector(elClass).style.backgroundColor = color;
+function backgroundColor(elements) {
+    elements.forEach(element => document.querySelector(element.className).style.backgroundColor = element.value);
 }
 
 function displayShow(elements) {
     elements.forEach(element => document.querySelector(element.className).style.display = element.value);
+}
+
+function mainStyleCircle(className, removeClass, addClass) {
+    document.querySelector(className).classList.remove(removeClass);
+    document.querySelector(className).classList.add(addClass);
+}
+function backgroundAnimation(param) {
+    document.querySelector('.circle').style.backgroundColor = param.backColor;
+    gsap.fromTo(".circle", {y: 500, x: -500, opacity: 0, padding: 30},
+        {y: 100, x: 300, opacity: 1, padding: 1500, duration: 1.2});
+}
+
+function putCardsInfo(card) {
+    gsap.fromTo(".content-card", {opacity: 0}, {opacity: 1, duration: 10, function() {
+            putCardsText(card);
+            backgroundAnimation(card)
+        }
+    });
 }
